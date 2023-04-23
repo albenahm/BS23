09e6d87536ba548feb53068903c2f2fdda6ae8fe
EricSprunk <130986329+EricSprunk@users.noreply.github.com>
2023-04-23 15:56:38 +0200

    Update main.c
    Erweitern der main.c für die for-Schleife

--- AS0/main.c
+++ AS0/main.c
@@ -2,10 +2,13 @@
 #include <stdbool.h>
 
 extern int whileSchleife(unsigned int eingabe);
+extern void forSchleife(int eingabe, int* result);
 
 int main(){
 
 long long int input;
+int result =0;
+     
 printf("Bitte geben Sie ein Zahl ein ! ");
 scanf("%llu",&input);
 printf("%llu \n",input);
@@ -15,7 +18,8 @@ if(0<input){
      if(input<4294967296){
 
         printf("While Schleife Ergebnis = %d \n",whileSchleife(input));
-
+        forSchleife(input,&result);
+        printf("For-Schleifen-Ergebnis = %d \n",result);
      }
 
 }else{
