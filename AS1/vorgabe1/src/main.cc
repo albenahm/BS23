c63b2ccae1ba2f43926c026a41e01fee137fb586
Ghostswordf <131553992+Ghostswordf@users.noreply.github.com>
2023-05-07 23:48:30 +0200

    Update main.cc
    Test Version

--- AS1/vorgabe1/src/main.cc
+++ AS1/vorgabe1/src/main.cc
@@ -1,27 +1,87 @@
 #include "device/CgaChannel.h"
+
 #include "io/PrintStream.h"
 
+
+
 CgaChannel cga;		// unser CGA-Ausgabekanal
+
 PrintStream out(cga);	// unseren PrintStream mit Ausgabekanal verknuepfen
 
+
+
 extern "C" int main()
+
 {
+
+
 
 	out.println("Hello World");
 
-	for(int i = 0; i < 1000; i++) {
-		out.print("Dezimal ");
-		out.print(i);
-		out.print(" Binaer ");
-		out.print(i, 2);
-		out.print(" Hexadezimal ");
-		out.print(i, 16);
-		out.println();
+	out.print(189,16);
+
+	out.println("");
+
+	out.print(0,10);
+
+	out.println("");
+
+	out.print(-4,2);
+
+	out.println("");
+
+	out.print(1000,16);
+
+	out.println("");
+
+	out.print(-2147483647,2);
+
+	out.println("");
+
+	out.print((unsigned) 4294967295,2);
+
+	out.println("");
+
+	for(int i = 0; i < 81; i++){
+
+		out.print("a");
+
 	}
 
-	out.println("done");
+	out.println("");
+
+	for(int i = 0; i < 7; i++){
+
+		out.print("a");
+
+	}
+
+	//out.print('\r');
+
+	for(int i = 0; i < 5; i++) {
+
+		out.print(i);
+
+	}
+
+	out.println("");
+
+	for(int i = 0; i < 5; i++){
+
+		out.print(i, 16);
+
+	}
+
+	out.println("");
+
+	out.print("done");
+
+	out.println();
 
 	while(1){}
 
+
+
 	return 0;
+
 }
