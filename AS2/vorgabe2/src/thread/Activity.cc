a0762501bbdb4127255ad11278f6fa68ee1b1885
albennahm <ahmad.bnee@gmail.com>
2023-05-19 08:43:32 -0700

    add Activity and update makefile

--- AS2/vorgabe2/src/thread/Activity.cc
+++ AS2/vorgabe2/src/thread/Activity.cc
@@ -1,47 +1,58 @@
 #include "thread/Activity.h"
 #include "thread/ActivityScheduler.h"
 
+/* Verpacken des aktuellen Kontrollflusses als Thread.
+* Wird nur f�r den Hauptkontrollfluss "main" ben�tigt.
+* Es wird hier kein Stack initialisiert.
+* Beachte das Activity wegen der Vererbungsbeziehung von
+* Coroutine abstrakt ist. Bei Bedarf muss "body" direkt
+* aufgerufen werden.
+*/
 Activity:: Activity(){
-    this->zustand = ZOMBIE;
+    this->zustand = BLOCKED; // nicht sofort ausfuehrbar, warte auf wakeup
     this->activitat=0;
     scheduler.start(this);
 }
 
-Activity:: Activity(void* tos){
+/* Aufsetzen eines Threads, der initiale Zustand ist "Blocked",
+* da der Thread erst laufen darf, wenn der spezielle Konstruktor
+* der abgeleiteten Klasse abgearbeitet ist. Die Aktivierung
+* erfolgt von der abgeleiteten Klasse mittels "wakeup".
+*/
+Activity:: Activity(void* tos):Coroutine(tos){
     this->zustand = BLOCKED;
     this->activitat=0;
-    this->Coroutine=tos;
 }
 
 Activity:: ~Activity(){
-    scheduler.kill(this);
+    scheduler.kill(this); // terminierung
 }
 
 	/* Veranlasst den Scheduler, diese Aktivitaet zu suspendieren.
 	 */
 	void Activity::sleep(){
-        scheduler.suspend();
+        scheduler.suspend();// Unterbrechung der laufenden Prozess
     }
 
 	/* Veranlasst den Scheduler, diese Aktivitaet aufzuwecken.
 	 */
 	void Activity::wakeup(){
         this->zustand=READY;
-        scheduler.schedule(this);
+        scheduler.schedule(this);// in ready list gebracht
     }
 
 	/* Diese Aktivitaet gibt die CPU vorruebergehend ab.
 	 */
 	void Activity::yield(){
-        scheduler.reschedule();
+        scheduler.reschedule(); // neuer Plan
     }
 
 	/* Diese Aktivitaet wird terminiert. Hier muss eine eventuell
 	 * auf die Beendigung wartende Aktivit�t geweckt werden.
 	 */
 	void Activity::exit(){
-        this->activitat != 0?this->activitat->wakeup();
-        scheduler.exit();
+        (this->activitat != 0)?this->activitat->wakeup(): // wenn Aktinitat nicht leer dann aufwachen
+        scheduler.exit(); // schliesen 
         
     }
 
@@ -50,10 +61,11 @@ Activity:: ~Activity(){
 	 * Wecken des wartenden Prozesses �bernimmt exit.
 	 */
 	void Activity::join(){
-        Activity* aktuellProzess=(Activity*) scheduler.active();
+        Activity* aktuellProzess=(Activity*) scheduler.active();// acktive Prozess in einer lokal Variable 
+        //Es existiert eine Aktivität und nicht derselbe laufende Prozess.
         if(!(this->isZombie())&& this !=aktuellProzess){
             this->activitat=(Activity*) scheduler.active();
-            scheduler.suspend();
+            scheduler.suspend();// Unterbrechung der laufenden Prozess
         }
         
     }
