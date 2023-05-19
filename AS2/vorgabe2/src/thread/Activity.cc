c1feb8757a69f3d75c0c2f16cb5a062d9726c2df
albennahm <ahmad.bnee@gmail.com>
2023-05-19 08:01:46 -0700

    fix makefile

--- AS2/vorgabe2/src/thread/Activity.cc
+++ AS2/vorgabe2/src/thread/Activity.cc
@@ -17,4 +17,43 @@ Activity:: ~Activity(){
     scheduler.kill(this);
 }
 
+	/* Veranlasst den Scheduler, diese Aktivitaet zu suspendieren.
+	 */
+	void Activity::sleep(){
+        scheduler.suspend();
+    }
 
+	/* Veranlasst den Scheduler, diese Aktivitaet aufzuwecken.
+	 */
+	void Activity::wakeup(){
+        this->zustand=READY;
+        scheduler.schedule(this);
+    }
+
+	/* Diese Aktivitaet gibt die CPU vorruebergehend ab.
+	 */
+	void Activity::yield(){
+        scheduler.reschedule();
+    }
+
+	/* Diese Aktivitaet wird terminiert. Hier muss eine eventuell
+	 * auf die Beendigung wartende Aktivit�t geweckt werden.
+	 */
+	void Activity::exit(){
+        this->activitat != 0?this->activitat->wakeup();
+        scheduler.exit();
+        
+    }
+
+	/* Der aktuelle Prozess wird solange schlafen gelegt, bis der
+	 * Prozess auf dem join aufgerufen wird beendet ist. Das
+	 * Wecken des wartenden Prozesses �bernimmt exit.
+	 */
+	void Activity::join(){
+        Activity* aktuellProzess=(Activity*) scheduler.active();
+        if(!(this->isZombie())&& this !=aktuellProzess){
+            this->activitat=(Activity*) scheduler.active();
+            scheduler.suspend();
+        }
+        
+    }
