f242428a9a4b8b681cb5c751a27e41fc203f992a
EricSprunk <130986329+EricSprunk@users.noreply.github.com>
2023-05-20 12:33:28 +0200

    Update Scheduler.cc
    scheduler geaendert

--- AS2/vorgabe2/src/thread/Scheduler.cc
+++ AS2/vorgabe2/src/thread/Scheduler.cc
@@ -1,13 +1,19 @@
+#include "lib/Queue.h"
+#include "thread/Scheduler.h"
+#include "thread/ActivityScheduler.h"
+#include "thread/Schedulable.h"
+
 //Einfuegen in Ready Liste
-void schedule(Schedulable* sched){
-    readyList.enqueue(sched);//Objekt schon erstellt in header(void Methode)
+void Scheduler::schedule(Schedulable* sched){
+   this->readylist.enqueue(sched);//Objekt schon erstellt in header(void Methode)
 };
 //Entfernen eines Elements aus der Ready Liste
-void remove(Schedulable* sched){
-    readyList.remove(sched);
+void Scheduler::remove(Schedulable* sched){
+    this->readylist.remove(sched);
 };
 //Aktiviert das vorderste Element
-void reschedule(){
-    scheduler.activate(readyList.dequeue());
+void Scheduler::reschedule(){
+   this->readylist.dequeue();
+   scheduler.activate(this);
 
 };
