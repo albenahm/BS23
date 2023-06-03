bbab4d9c9294fd97abe73a48c063c6970c264906
Ghostswordf <131553992+Ghostswordf@users.noreply.github.com>
2023-06-03 15:24:48 +0200

    Update Scheduler.cc
    IntLock eingefügt

--- AS3/vorgabe3/src/thread/Scheduler.cc
+++ AS3/vorgabe3/src/thread/Scheduler.cc
@@ -2,14 +2,20 @@
 
 //Einfuegen in Ready Liste
 void Scheduler::schedule(Schedulable* sched){
+    
+    IntLock lock;
     readylist.enqueue(sched);//Objekt schon erstellt in header(void Methode)
 };
 //Entfernen eines Elements aus der Ready Liste
 void Scheduler::remove(Schedulable* sched){
+    
+    IntLock lock;
     readylist.remove(sched);
 };
 //Aktiviert das vorderste Element
 void Scheduler::reschedule(){
+    
+    IntLock lock;
     activate((Schedulable*) readylist.dequeue());
 
 };
