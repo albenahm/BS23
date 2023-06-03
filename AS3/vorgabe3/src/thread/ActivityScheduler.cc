9d981b2c873a0cd68a2de1b05c6fee82476a6970
Ghostswordf <131553992+Ghostswordf@users.noreply.github.com>
2023-06-03 15:22:03 +0200

    Update ActivityScheduler.cc
    IntLock eingefuegt + Klammer gefixt

--- AS3/vorgabe3/src/thread/ActivityScheduler.cc
+++ AS3/vorgabe3/src/thread/ActivityScheduler.cc
@@ -1,12 +1,5 @@
-//#include "thread/Scheduler.cc" // .cc ?
-
 #include "thread/ActivityScheduler.h"
-
 #include "thread/Dispatcher.h"
-
-
-
-
 
 /* Suspendieren des aktiven Prozesses
 
@@ -18,103 +11,72 @@
 
 void ActivityScheduler::suspend(){
 
+	IntLock lock;
     	((Activity *)active())-> changeTo(Activity::BLOCKED); // derzeitiger Prozess wird geblockt (suspendiert)
-
 	this->reschedule();
-
-
-
 };
 
-
-
 /* Explizites Terminieren des angegebenen Prozesses. Der Prozesszustand ist korrekt zu setzen und der Prozess aus der Ready-Liste des Schedulers
-
  * zu entfernen. Falls der aktive Prozess zerstoert wird, ist dem naechsten lauffaehigen Prozess die CPU zuzuteilen.
-
  */
 
 void ActivityScheduler::kill(Activity*  actActivity){
 
+	IntLock lock;
 	actActivity-> changeTo(Activity::ZOMBIE); // derzeitiger Prozess wird zerstört (Zombie)
-
 	(actActivity==(Activity *)active())?this->reschedule():remove(actActivity); // falls die act laeyt, dann fuehre die naechste aus der Liste aus.
-
 };
 
-
-
-
-
 /* Terminieren des aktiven Prozesses,
-
 	 * und Wechsel zum naechsten lauffaehigen Prozess
-
 	 */
 
 void ActivityScheduler::exit(){
 
+	IntLock lock;
 	Activity*  actActivity= (Activity*)this->active();// bekomme die aktuelle Aktivitaet
-
 	actActivity-> changeTo(Activity::ZOMBIE);
-
 	reschedule(); // hole die neue
-
 }
 
-
-
 /* Der aktive Prozess ist, sofern er sich nicht im Zustand Blocked oder Zombie befindet, wieder auf die Ready-Liste
-
  * zu setzen. Danach ist "to" mittels dispatch die Kontrolle zu �bergeben.
-
  */
 
 void ActivityScheduler::activate(Schedulable* to){
 
+	Activity* actuellActivity = (Activity*) active();
+	Activity* nextActivity = (Activity*) to;
 
-
-Activity* actuellActivity = (Activity*) active();
-
-Activity* nextActivity = (Activity*) to;
-
-
-//hier prüfe,ob die actuelle Aktivitä aktiv ist
-if(actuellActivity -> isRunning()&&(!(actuellActivity -> isBlocked())||(actuellActivity -> isZombie()))){
+	//hier prüfe,ob die actuelle Aktivitä aktiv ist
+	if(actuellActivity -> isRunning()&&(!(actuellActivity -> isBlocked())||(actuellActivity -> isZombie()))){
 	//wenn ja setze aktuelle Aktivitaet auf bereit und in Queue(bereitliste)
-	actuellActivity->changeTo(Activity::READY);
-	scheduler.schedule(actuellActivity);
-
-}
-
-//In diesem Punkt haben wir die laufende Aktivität deaktiviert.
-
-//nun Suche nach die nächste Aktivität im Fall die akutelle nicht aktiv
-while(nextActivity==0){
+		actuellActivity->changeTo(Activity::READY);
+		scheduler.schedule(actuellActivity);
+	}
+	//In diesem Punkt haben wir die laufende Aktivität deaktiviert.
+	//nun Suche nach die nächste Aktivität im Fall die akutelle nicht aktiv
+	while(nextActivity==0){
 		
-	if (! actuellActivity->isRunning()||(actuellActivity -> isBlocked()||(actuellActivity -> isZombie()))){
+		if (! actuellActivity->isRunning()||(actuellActivity -> isBlocked()||(actuellActivity -> isZombie()))){
 	//hole eine von Queue(Aktivitätsiste
-	nextActivity =  (Activity*) readylist.dequeue();}
+		nextActivity =  (Activity*) readylist.dequeue();
 		}
+	}
+	//wenn die geholte Aktivität schon exisitiert ist, dann wird sie ausgeführt.
+	if(nextActivity!=0){
+	nextActivity->changeTo(Activity::RUNNING);
+	dispatch(nextActivity);// Zeige auf denn aktelle laufenden Prozess
+	}
+}	
 
+void ActivityScheduler::checkSlice(){
 
-//wenn die geholte Aktivität schon exisitiert ist, dann wird sie ausgeführt.
-if(nextActivity!=0){
-nextActivity->changeTo(Activity::RUNNING);
-
-dispatch(nextActivity);// Zeige auf denn aktelle laufenden Prozess
-}
-	
-
-void ActivityScheduler:: checkSlice(){
-
-Activity* actuellActivity = (Activity*) active();
-// quatum() ist das max Zeit bei dem gewechselt wird . ticks() liefert die aktuelle Zeit
-if((actuellActivity->quantum()) <= (actuellActivity-> ticks())){
-this->reschedule();
-}
-	
-}
+	IntLock lock;
+	Activity* actuellActivity = (Activity*) active();
+	// quatum() ist das max Zeit bei dem gewechselt wird . ticks() liefert die aktuelle Zeit
+	if((actuellActivity->quantum()) <= (actuellActivity-> ticks())){
+		this->reschedule();
+	}
 	
 }
-
